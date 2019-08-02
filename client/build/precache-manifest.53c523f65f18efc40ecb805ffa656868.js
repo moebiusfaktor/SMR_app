e5862a377848447f00673ee7bc038b0de9125c0a
moebiusfaktor <alexmoebius13@gmail.com>
2019-08-02 02:22:29 +0200

    built

---
+++ client/build/precache-manifest.53c523f65f18efc40ecb805ffa656868.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64bfa844206cc36c1326755468e9a60d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afb9a9ca7d231136e828",
+    "url": "/static/css/main.637d0d7c.chunk.css"
+  },
+  {
+    "revision": "2a8dd3d0cf53e67da984",
+    "url": "/static/js/2.4fa0193f.chunk.js"
+  },
+  {
+    "revision": "afb9a9ca7d231136e828",
+    "url": "/static/js/main.13263930.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
